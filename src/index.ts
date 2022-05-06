2157f167cf0fef45c59928d02522c60369c8844e
us869 <42154943+us869@users.noreply.github.com>
2022-05-06 17:55:33 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { startBasicScene } from './basic_scene';
-import { startDingbatsScene } from './dingbats_scene';
-import { startStarWarsScene } from './star_wars_scene'
+//import { startDingbatsScene } from './dingbats_scene';
+//import { startStarWarsScene } from './star_wars_scene'
 
 startBasicScene();
 // startStarWarsScene();
